7fcb94cfa55144d4a329777e1366377cd91f8851
경태윤 <xodbsrud320@gmail.com>
2021-11-15 14:47:24 +0900

    5th

--- src/com/javaex/ex02/Ex14.java
+++ src/com/javaex/ex02/Ex14.java
@@ -19,6 +19,7 @@ public class Ex14 {
 		System.out.println("당신의 이름은 " + name + " 나이는 " + age + " 입니다.");
 		System.out.println("깃 테스트 버전02");
 		System.out.println("깃 테스트 버전04");
+		System.out.println("깃 테스트 버전05");
 		
 		sc.close();
 	}
